53e59898060d2165419aa085aa44893be16b20df
Okan Tümüklü <67555432+okantumuklu@users.noreply.github.com>
2021-08-17 17:31:08 +0300

    C dilinde merhaba dünya.
    ;)

---
+++ merhabadünya.c
@@ -0,0 +1,12 @@
+// Program Linux Debian 10 buster işletim sisteminde yazılmıştır.
+// kütüphane dahil ediyoruz
+#include <stdio.h>
+
+// ana fonksiyonumuz
+int main()
+{
+	//Printf ile ekrana yazdırma işmei ve \n ie alt satıra geçiyor
+	printf("Hello world!\n");
+	// programın sonlandığını anlatıyoruz
+	return 0;
+}
