83835a30e2a71667150b614f35cb4e04fcbde7c1
Okan Tümüklü <67555432+okantumuklu@users.noreply.github.com>
2021-08-17 17:40:04 +0300

    DegiskenBastirma.c
    Okan Tümüklü

---
+++ DegiskenBastirma.c
@@ -0,0 +1,16 @@
+// Program linux işletim sistemi tarafından yazılmıştır.
+
+// Kütüphane ekliyoru.
+#include<stdio.h>
+
+// Ana fonksiyon
+int main()
+{
+	//değişken atıyoruz a bizim için 5 olacak
+	int a = 5;
+	// Tam sayı değişkenini erkana bastırıyoruz.
+	printf("%d",a);
+	
+	//Programın bittiğini söylüyoruz.
+	return 0;
+}
